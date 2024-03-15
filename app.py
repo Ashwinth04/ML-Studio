c42d1f51b40b7c563fdff27105ff0901ad304ac9
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2024-03-15 17:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,27 @@ Drag, drop, and build powerful ML models. Our intuitive studio empowers everyone
 
 # Side bar
 st.sidebar.subheader("Setup your Problem Here")
-problem_type = st.sidebar.selectbox("Pick your Problem Type", ["Regression", "Classification", "Clustering"])
+problem_type = st.sidebar.selectbox("Pick your Problem Type", ["Regression", "Classification", "Clustering"])\
+
+if problem_type == "Regression":
+    state = 1
+elif problem_type == "Classification":
+    state = 2
+elif problem_type == "Clustering":
+    state = 3
+else:
+    state = 4
+
 dataset_file = st.sidebar.file_uploader("Upload your Dataset")
-if problem_type != "Clustering":
+st.subheader("Your Dataset:-")
+df = pd.read_csv(dataset_file)
+st.dataframe(df)
+if state == 1 or state == 2:
     target_y = st.sidebar.text_input("Enter the Target Variable Column Name")
 
 
 if st.sidebar.button("Train"):
-    st.subheader("Your Dataset:-")
-    df = pd.read_csv(dataset_file)
-    st.dataframe(df)
-    if problem_type != "Clustering" and target_y != "":
+    if (state == 1 or state == 2) and target_y != "":
         cols = list(df.columns.values)
         cols.pop(cols.index(target_y))
         df = df[cols+[target_y]]
@@ -31,39 +41,107 @@ if st.sidebar.button("Train"):
     model = Models(df)
     model.clean_and_scale_dataset()
 
-    if problem_type == "Regression":
-        regression_models = ["Linear Regression", "DTree Regression", "SVR"]
+    if state == 1:
+        regression_models = ["Linear Regression", "Decision Tree Regression", "SVR", "Ridge Regression", "Lasso Regression",
+                             "ElasticNet", "Random Forest Regressor", "Multi-Layer Perceptron", "KNN Regressor", 
+                             "Gradient Boosting Regressor"]
         metrics = ["R2-Score"]
         lr = list(model.linear_regression())
         dtr = list(model.dtree_regressor())
         svr = list(model.SVR())
+        rr = list(model.ridge_regression())
+        lsr = list(model.lasso())
+        en = list(model.elasticnet())
+        rfr = list(model.random_forest_regressor())
+        mlpr = list(model.mlp_regressor())
+        knnr = list(model.knn_regressor())
+        gbr = list(model.gradient_boost_regressor())
+
+        regression_funcs = {
+            "Linear Regression": lr[0],
+            "Decision Tree Regression": dtr[0],
+            "SVR": svr[0],
+            "Ridge Regression": rr[0],
+            "Lasso Regression": lsr[0],
+            "ElasticNet": en[0],
+            "Random Forest Regressor": rfr[0],
+            "Multi-Layer Perceptron": mlpr[0],
+            "KNN Regressor": knnr[0],
+            "Gradient Boosting Regressor": gbr[0]
+        }
 
         regressors_table = {
             "Linear Regression": lr[1:],
             "Decision Tree Regression": dtr[1:],
-            "SVR": svr[1:]
+            "SVR": svr[1:],
+            "Ridge Regression": rr[1:],
+            "Lasso Regression": lsr[1:],
+            "ElasticNet": en[1:],
+            "Random Forest Regressor": rfr[1:],
+            "Multi-Layer Perceptron": mlpr[1:],
+            "KNN Regressor": knnr[1:],
+            "Gradient Boosting Regressor": gbr[1:]
         }
         comp_table = pd.DataFrame.from_dict(regressors_table)
         comp_table.index = metrics
+        comp_table = comp_table.transpose()
 
-    elif problem_type == "Classification":
-        regression_models = ["SVM", "Logistic Regression", "Decision Tree", "KNN"]
+    elif state == 2:
+        classification_models = ["SVM", "Logistic Regression", "Decision Tree", "KNN", "Naive Bayes", 
+                                 "AdaBoost", "Multi-Layer Perceptron", "Gradient Boosting", "Random Forest"]
         metrics = ["Accuracy"]
-        svm = list(model.SVM())
+        s = list(model.SVM())
         lor = list(model.logistic_regression())
-        dt = list(model.decision_tree())
-        knn = list(model.knn())
+        dt = list(model.dtree_classifier())
+        knn = list(model.knn_classifier())
+        nb = list(model.naivebayes())
+        ab = list(model.adaboost())
+        mlp = list(model.mlp())
+        gb = list(model.gradient_boost())
+        rf = list(model.random_forest_classifier())
+
+        classification_funcs = {
+            "SVM": s[0],
+            "Logistic Regression": lor[0],
+            "Decision Tree": dt[0],
+            "KNN": knn[0],
+            "Naive Bayes": nb[0],
+            "AdaBoost": ab[0],
+            "Multi-Layer Perceptron": mlp[0],
+            "Gradient Boosting": gb[0],
+            "Random Forest": rf[0]
+        }
 
         classifiers_table = {
-            "SVM": svm[1:],
+            "SVM": s[1:],
             "Logistic Regression": lor[1:],
             "Decision Tree": dt[1:],
-            "KNN": knn[1:]
+            "KNN": knn[1:],
+            "Naive Bayes": nb[1:],
+            "AdaBoost": ab[1:],
+            "Multi-Layer Perceptron": mlp[1:],
+            "Gradient Boosting": gb[1:],
+            "Random Forest": rf[1:]
         }
         comp_table = pd.DataFrame.from_dict(classifiers_table)
         comp_table.index = metrics
+        comp_table = comp_table.transpose()
+    elif state == 3:
+        pass
     else:
         pass
 
     st.subheader("Comparision Table")
     st.dataframe(comp_table)
+
+    st.sidebar.subheader("Test your Model Here")
+    if state == 1:
+        model_name = st.sidebar.selectbox("Pick your Model", ["Best Model"] + regression_models)
+    elif state == 2:
+        model_name = st.sidebar.selectbox("Pick your Model", ["Best Model"] + classification_models)
+    elif state == 3:
+        pass
+    else:
+        pass
+    
+    
--- temp_model.py
+++ temp_model.py
@@ -1,102 +1,253 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn import tree
 from sklearn import svm
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, RandomForestRegressor, GradientBoostingRegressor
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 import pandas as pd
 
-
 class Models:
-    def __init__(self, dataframe):
+    def __init__(self,dataframe):
         self.df = dataframe
         self.X = None
         self.y = None
 
     def clean_and_scale_dataset(self):
         self.df = self.df.dropna()
-        self.X = self.df.drop(columns=self.df.columns[-1])
-        self.y = self.df.iloc[:, -1]
+        self.X = self.df.drop(columns = self.df.columns[-1])
+        self.y = self.df.iloc[:,-1]
         scaler = StandardScaler()
         self.X = scaler.fit_transform(self.X)
-        return self.X, self.y
-
+        return self.X,self.y
+    
+    ####################### Classifiers ################################
     def SVM(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         classifier = svm.SVC()
-        classifier.fit(X_train, y_train)
+        classifier.fit(X_train,y_train)
         y_pred = classifier.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return classifier, accuracy
-
+        accuracy = accuracy_score(y_test,y_pred)
+        return classifier,accuracy
+    
     def logistic_regression(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-        classifier = LogisticRegression()
-        classifier.fit(X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier =  LogisticRegression()
+        classifier.fit(X_train,y_train)
         y_pred = classifier.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return classifier, accuracy
-
-    def decision_tree(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        accuracy = accuracy_score(y_test,y_pred)
+        return classifier,accuracy
+    
+    def dtree_classifier(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         classifier = tree.DecisionTreeClassifier()
-        classifier.fit(X_train, y_train)
+        classifier.fit(X_train,y_train)
         y_pred = classifier.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return classifier, accuracy
-
-    def knn(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        accuracy = accuracy_score(y_test,y_pred)
+        return classifier,accuracy
+    
+    def knn_classifier(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         classifier = KNeighborsClassifier()
-        classifier.fit(X_train, y_train)
+        classifier.fit(X_train,y_train)
         y_pred = classifier.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return classifier, accuracy
+        accuracy = accuracy_score(y_test,y_pred)
+        return classifier,accuracy
+    
+    def naivebayes(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier = GaussianNB()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        accuracy = accuracy_score(y_pred,y_test)
+        return classifier,accuracy
+    
+    def adaboost(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier = AdaBoostClassifier()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        accuracy = accuracy_score(y_pred,y_test)
+        return classifier,accuracy
+    
+    def mlp(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier = MLPClassifier()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        accuracy = accuracy_score(y_pred,y_test)
+        return classifier,accuracy
+    
+    def gradient_boost(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier = GradientBoostingClassifier()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        accuracy = accuracy_score(y_pred,y_test)
+        return classifier,accuracy
+    
+    def random_forest_classifier(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        classifier = RandomForestClassifier()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        accuracy = accuracy_score(y_pred,y_test)
+        return classifier,accuracy
+    
 
+    ##########################  Regressors ##################################
     def linear_regression(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         regressor = LinearRegression()
-        regressor.fit(X_train, y_train)
+        regressor.fit(X_train,y_train)
         y_pred = regressor.predict(X_test)
-        score = r2_score(y_test, y_pred)
-        return regressor, score
-
+        score = r2_score(y_test,y_pred)
+        return regressor,score
+    
     def dtree_regressor(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         regressor = tree.DecisionTreeRegressor()
-        regressor.fit(X_train, y_train)
+        regressor.fit(X_train,y_train)
         y_pred = regressor.predict(X_test)
-        score = r2_score(y_test, y_pred)
-        return regressor, score
-
+        score = r2_score(y_test,y_pred)
+        return regressor,score
+    
     def SVR(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
         regressor = svm.SVR()
-        regressor.fit(X_train, y_train)
+        regressor.fit(X_train,y_train)
         y_pred = regressor.predict(X_test)
-        score = r2_score(y_test, y_pred)
+        score = r2_score(y_test,y_pred)
+        return regressor,score
+    
+    def ridge_regression(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = Ridge()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_test,y_pred)
+        return regressor,score
+    
+    def lasso(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = Lasso()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
         return regressor, score
+    
+    def elasticnet(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = ElasticNet()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
+        return regressor, score
+    
+    def random_forest_regressor(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = RandomForestRegressor()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
+        return regressor,score
+    
+    def mlp_regressor(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = MLPRegressor()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
+        return regressor, score
+    
+    def knn_regressor(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = KNeighborsRegressor()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
+        return regressor, score
+    
+    def gradient_boost_regressor(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+        regressor = GradientBoostingRegressor()
+        regressor.fit(X_train,y_train)
+        y_pred = regressor.predict(X_test)
+        score = r2_score(y_pred,y_test)
+        return regressor,score
+    
+    ########################## Clustering ####################################
 
-    def predict(self, clf, x):
-        answer = clf.predict(x)
+    def kmeans(self,num_clusters):
+        X_scaled, _ = self.clean_and_scale_dataset()
+        kmeans = KMeans(n_clusters=num_clusters,random_state=42)
+        labels = kmeans.fit_predict(X_scaled)
+        return kmeans, labels
+    
+    def agglomerative_clustering(self,num_clusters):
+        X_scaled, _ = self.clean_and_scale_dataset()
+        agg = AgglomerativeClustering(n_clusters=num_clusters)
+        labels = agg.fit_predict(X_scaled)
+        return agg, labels
+    
+    def dbscan(self,eps=0.5, min_samples = 5):
+        X_scaled, _ = self.clean_and_scale_dataset()
+        dbscan = DBSCAN(eps = eps,min_samples=min_samples)
+        labels = dbscan.fit_predict(X_scaled)
+        return dbscan, labels
+
+    def predict(self,model,x):
+        answer = model.predict(x)
         return answer
-
+    
     def pick_best_classifier(self):
-        model_name, model, opt_score = "", None, 0
+        model_name,model,opt_score = "",None,0
 
         classifiers = {
-            "SVM": self.SVM(),
-            "Logistic Regression": self.logistic_regression(),
-            "Decision Tree": self.decision_tree(),
-            "KNN": self.knn()
-        }
-
-        for name, tup in classifiers.items():
-            clf, score = tup
-            if (score > opt_score):
+            "SVM":self.SVM(),
+            "Logistic Regression":self.logistic_regression(),
+            "Decision Tree": self.dtree_classifier(),
+            "KNN":self.knn_classifier(),
+            "Naive Bayes": self.naivebayes(),
+            "Ada boost": self.adaboost(),
+            "Gradient boost": self.gradient_boost(),
+            "Random Forest": self.random_forest_classifier(),
+        }      
+        
+        for name,tup in classifiers.items():
+            clf,score = tup
+            if(score > opt_score):
                 opt_score = score
                 model = clf
                 model_name = name
 
-        return model_name, model, opt_score
+        return classifiers, model_name, model, opt_score
+    
+    def pick_best_regressor(self):
+        model_name, model, opt_score = "",None,0
+
+        regressors = {
+            "Linear Regression": self.linear_regression(),
+            "Decision tree regression": self.dtree_regressor(),
+            "Support vector regression": self.SVR(),
+            "Ridge regression":self.ridge_regression(),
+            "Lasso regression":self.lasso(),
+            "ElasticNet regression":self.elasticnet(),
+            "Random Forest regression":self.random_forest_regressor(),
+            "Multi-layer perceptron":self.mlp_regressor(),
+            "KNN":self.knn_regressor(),
+            "Gradient Boosting regression":self.gradient_boost_regressor()
+        }
+
+        for name,tup in regressors.items():
+            clf,score = tup
+            if(score > opt_score):
+                opt_score = score
+                model = clf
+                model_name = name
+
+        return regressors, model_name, model, opt_score
